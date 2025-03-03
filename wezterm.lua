67639dceb1ba2624f465c3b4862f055688ceac8e
Govardhanen Gopal <govardha@gmail.com>
2025-03-03 14:10:32 -0500

    More Changes

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 local act = wezterm.action
+local mux = wezterm.mux
 
 -- These are vars to put things in later (i dont use em all yet)
 local keys = {}
@@ -9,9 +10,21 @@ local mouse_bindings = {}
 local config = wezterm.config_builder()
 local launch_menu = {}
 
+-- stufff for custom shit
+local user_name = os.getenv('USERNAME') or os.getenv('USER') or os.getenv('LOGNAME')
+local user_domain = os.getenv('USERDOMAIN')
+local msys_ssh = "C:/DevSoftware/msys64/usr/bin/ssh.exe"
+local win_ssh = "C:/Windows/System32/OpenSSH/ssh.exe"
+local ssh_id_file = "C:\\users\\" .. user_name .. "\\.ssh\\id_ed25519"
+-- local ssh_id_file = 
+
 -- This will hold the configuration.
 -- Basic stuff
 -- https://github.com/wezterm/wezterm/discussions/4728
+
+-- local is_work <const> = string.find(os.getenv('USERDOMAIN')"MIAMIHOLDINGS") ~= nil
+-- local is_work <const> = os.getenv('USERDOMAIN'):find("MIAMIHOLDINGS") ~= nil
+
 
 local is_linux = function()
 	return wezterm.target_triple:find("linux") ~= nil
@@ -25,6 +38,31 @@ local is_windows = function()
 	return wezterm.target_triple:find("windows") ~= nil
 end
 
+-- define different ssh luas for home vs work
+-- if is_work then
+--   local ssh_domains = require('extra.ssh')
+-- else
+--   local ssh_domains = require('extra.ssh-home')
+--   -- local msys_ssh = "C:/Apps/msys64/usr/bin/ssh.exe"
+-- end
+
+-- local ssh_domains = require('extra.ssh-home')
+
+-- local dom = os.getenv("USERDOMAIN")
+-- require(strings.lower(dom))
+
+config.ssh_domains = wezterm.default_ssh_domains()
+
+for i, dom in ipairs(config.ssh_domains) do
+  dom.assume_shell = 'Posix'
+  if dom.name and string.match(dom.name ,"SSHMUX:what") then
+    dom.remote_wezterm_path = "/home/gundan/bin/wezterm"
+  else
+    dom.remote_wezterm_path = "/home/ubuntu/bin/wezterm"
+  end
+  config.ssh_domains[i] = dom
+end
+
 config.launch_menu = launch_menu
 
 config.set_environment_variables = {
@@ -34,39 +72,193 @@ config.set_environment_variables = {
   prompt = '$E]7;file://localhost/$P$E\\$E[32m$T$E[0m $E[35m$P$E[36m$_$G$E[0m ',
 }
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-   config.font = wezterm.font('Consolas')
+-- config.color_scheme = 'Dark Ocean (terminal.sexy)'
+-- config.color_scheme = 'Hardcore'
+-- config.color_scheme = 'Tomorrow Night Bright'
+config.color_scheme = 'Google (light) (terminal.sexy)'
+config.color_scheme = 'Cai (Gogh)'
+config.color_scheme = 'Tokyo Night Light (Gogh)'
+config.color_scheme = 'Bamboo'
+config.color_scheme = 'Tokyo Night'
+config.color_scheme = 'Tokyo Night Storm'
+
+-- First no work nonsense
+-- gov home shit
+if (wezterm.target_triple == "x86_64-pc-windows-msvc" and not is_work ) then
+  
+  config.font = wezterm.font('JetBrains Mono')
+  config.canonicalize_pasted_newlines = 'CarriageReturn'
   table.insert(launch_menu, {
-	  label = "Cmd",
+	  label = "cmd",
    	  args = { "cmd.exe" },
 	})
   table.insert(launch_menu, {
 	  label = "---",
 	})
   table.insert(launch_menu, {
-	  label = "rd2-ts",
-   	  args = { "C:/Windows/System32/OpenSSH/ssh.exe","ubuntu@rinku-depot2.pigeon-hamlet.ts.net"},
+	  label = "msys",
+	  args = { "cmd.exe ", "/k", "C:\\Apps\\msys64\\msys2_shell.cmd -defterm -here -no-start -msys2 -shell bash" },
 	})
   table.insert(launch_menu, {
-	  label = "rd2",
-   	  args = { "C:/Windows/System32/OpenSSH/ssh.exe","ubuntu@rinku-depot2.vadai.org"},
+	  label = "ucrt64",
+	  args = { "cmd.exe ", "/k", "C:\\Apps\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" },
+	})
+    config.default_prog = { "cmd.exe" }
+-- Then work nonsense
+elseif (wezterm.target_triple == "x86_64-pc-windows-msvc" and is_work) then
+  config.font = wezterm.font('JetBrains Mono')
+  -- Added this so that copy/paste would work normally in windows
+  config.canonicalize_pasted_newlines = 'CarriageReturn'
+  config.background = {
+    {
+      source = {
+        File = "C:/DevSoftware/WezTerm/images/miax-background.png",
+      },
+      opacity = 0.2,
+      -- height = 50%,
+      -- width = 50%,
+    },
+  }
+
+  config.default_prog = { "cmd.exe" }
+  table.insert(launch_menu, {
+	  label = "cmd",
+   	  args = { "cmd.exe" },
 	})
   table.insert(launch_menu, {
-	  label = "what",
-   	  args = { "C:/Windows/System32/OpenSSH/ssh.exe","gundan@olive.whatbox.ca"},
+	  label = "--- Prod GW ---",
+	})
+  table.insert(launch_menu, {
+	  label = "c2-msys",
+   	  args = {
+   	   msys_ssh,
+       user_name .. "@dch4i1gws02"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "c2-win",
+   	  args = {
+   	   win_ssh,
+       user_name .. "@dch4i1gws02"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "c1-msys",
+   	  args = {
+   	   msys_ssh,
+       user_name .. "@dch4i1gws01"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "c1-win",
+   	  args = {
+   	   win_ssh,
+       user_name .. "@dch4i1gws01"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "n1-msys",
+   	  args = {
+   	   msys_ssh,
+       user_name .. "@dny2i1gws01"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "n2-msys",
+   	  args = {
+   	   msys_ssh,
+       user_name .. "@dny2i1gws02"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "n1-win",
+   	  args = {
+   	   win_ssh,
+       user_name .. "@dny2i1gws01"
+      }
+	})
+  table.insert(launch_menu, {
+	  label = "n2-win",
+   	  args = {
+   	   win_ssh,
+       user_name .. "@dny2i1gws02"
+      }
+	})
+  -- table.insert(launch_menu, {
+	--   label = "bsx",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dbm2i1gws01"
+  --     }
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "--- Dev ---",
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "bds16",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1bds16-em2"
+  --     }
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "bds48",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1bds48-em2"
+  --     }
+  --  	  -- args = {'C:/Windows/System32/OpenSSH/ssh.exe','ggopal@dny2d1bds16-em2'},
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "bds60",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1bds60-em2"
+  --     }
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "sds01",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1sds01-em2"
+  --     }
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "sds02",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1sds02-em2"
+  --     }
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "sds03",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1sds03-em2"
+  --     }
+	-- })
+  -- table.insert(launch_menu, {
+	--   label = "sds04",
+  --  	  args = {
+  --  	   msys_ssh,
+  --      user_name .. "@dny2d1sds04-em2"
+  --     }
+	-- })
+  table.insert(launch_menu, {
+	  label = "--- Utils ---",
+	})
+  table.insert(launch_menu, {
+	  label = "ucrt64",
+	  args = { "cmd.exe ", "/k", "C:\\DevSoftware\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" },
+	})
+  table.insert(launch_menu, {
+	  label = "msys2",
+	  args = { "cmd.exe ", "/k", "C:\\DevSoftware\\msys64\\msys2_shell.cmd -defterm -here -no-start -msys2 -shell bash" },
 	})
   table.insert(launch_menu, {
 	  label = "---",
 	})
-  table.insert(launch_menu, {
-	  label = "MSYS UCRT64",
-	  args = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" },
-	})
-	--config.default_prog	= { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" }
-    config.default_prog = { "cmd.exe" }
 elseif wezterm.target_triple == "aarch64-apple-darwin" then
-  -- config.font = wezterm.font('Courier New')
-  -- config.font = wezterm.font('Consolas')
   config.font = wezterm.font('JetBrains Mono')
   table.insert(launch_menu, {
 	  label = "terminal",
@@ -102,20 +294,37 @@ config.window_background_gradient = {
   -- Linear and Radial gradients are also supported; see the other
   -- examples below
   orientation = 'Vertical',
+  orientation = 'Horizontal',
 
   -- Specifies the set of colors that are interpolated in the gradient.
   -- Accepts CSS style color specs, from named colors, through rgb
   -- strings and more
+  -- colors = {
+  --   '#009B93',
+  --   '#2ABC24',
+  --   '#F15822',
+  --   '#414042',
+  --   '#1A1A1A',
+  -- },
+  
+  -- colors = {
+  --   '#F15822',
+  --   '#009E9F',
+  --   '#2A8C24',
+  --   '#015EFF',
+  -- },
+
   colors = {
-    '#0f0c29',
-    '#302b63',
-    '#24243e',
+	 '#000000',
+	 '#191970'
   },
+
 
   -- Instead of specifying `colors`, you can use one of a number of
   -- predefined, preset gradients.
   -- A list of presets is shown in a section below.
-  -- preset = "BuPu",
+  -- preset = "CubeHelixDefault",
+  -- preset = "Cividis",
 
   -- Specifies the interpolation style to be used.
   -- "Linear", "Basis" and "CatmullRom" as supported.
@@ -133,7 +342,7 @@ config.window_background_gradient = {
   -- Smaller values, or 0, will make bands more prominent.
   -- The default value is 64 which gives decent looking results
   -- on a retina macbook pro display.
-  -- noise = 64,
+  noise = 25,
 
   -- By default, the gradient smoothly transitions between the colors.
   -- You can adjust the sharpness by specifying the segment_size and
@@ -175,8 +384,8 @@ config.tab_bar_style = {
 }
 
 config.window_frame = {
-  -- font = require('wezterm').font 'Roboto',
-  font = require('wezterm').font 'JetBrains Mono',
+  -- font = require('wezterm').font 'JetBrains Mono',
+  font = require('wezterm').font 'Roboto',
   font_size = 10,
   border_left_width = '0.5cell',
   border_right_width = '0.5cell',
@@ -217,42 +426,42 @@ mouse_bindings = {
     action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
     mods = 'NONE',
   },
- {
-  event = { Down = { streak = 1, button = "Right" } },
-  mods = "NONE",
-  action = wezterm.action_callback(function(window, pane)
-   local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-   if has_selection then
-    window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-    window:perform_action(act.ClearSelection, pane)
-   else
-    window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-   end
-  end),
- },
+  {
+		event = { Down = { streak = 1, button = 'Right' } },
+		mods = "NONE",
+    action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
 }
 
-config.window_background_opacity = 0.88
+config.window_background_opacity = 0.95
 
 -- This is used to make my foreground (text, etc) brighter than my background
 config.foreground_text_hsb = {
   hue = 1.0,
-  saturation = 1.0,
+  saturation = 1.5,
   brightness = 1.9,
 }
 
-wezterm.on(
-  'log-selection', 
-  function(window, pane)
-    local sel = window:get_selection_text_for_pane(pane)
-    local str = sel:gsub("\r?\n", "")
-    window:copy_to_clipboard(str, 'Clipboard')
-  end
-)
+-- wezterm.on(
+--   'log-selection', 
+--   function(window, pane)
+--     local sel = window:get_selection_text_for_pane(pane)
+--     local str = sel:gsub("\r?\n", "")
+--     window:copy_to_clipboard(str, 'Clipboard')
+--   end
+-- )
 
 config.keys = {
   -- to make sure tmux copy/paste cleans up new line
-  { key = "c", mods = "SUPER", action = act.EmitEvent 'log-selection' },
+  -- { key = "c", mods = "SUPER", action = act.EmitEvent 'log-selection' },
   { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
   { key = ',',
     mods = 'LEADER',
@@ -302,13 +511,91 @@ wezterm.on(
         { Text = '[' .. tab.tab_index + 1 .. '] ' .. title },
       }
     end
-    return title
   end
 )
 
--- Tmux shit
+-- Spawn 4 tabs
+-- if is_work then
+-- wezterm.on('gui-startup', function()
+--   -- default shit
+--   local _, first_pane, prod_window = mux.spawn_window {
+--     workspace = 'prod'
+--   }
+--   local _, _, _ = prod_window:spawn_tab {
+--     args = { "cmd.exe ", "/k", "C:\\DevSoftware\\msys64\\msys2_shell.cmd -defterm -here -no-start -msys2 -shell bash" },
+--   }
+--   -- local _, _, _ = prod_window:spawn_tab {
+--   --   workspace = 'prod',
+--   --     args = {
+--   --      msys_ssh,
+--   --      user_name .. "@dch4i1gws02"
+--   --     }
+--   -- }
+--   -- local _, _, _ = prod_window:spawn_tab {
+--   --   workspace = 'prod',
+--   --     args = {
+--   --      msys_ssh,
+--   --      user_name .. "@dch4i1gws02"
+--   --     }
+--   -- }
+
+--   -- dev shit
+--   local _, _, dev_window = mux.spawn_window {
+--     workspace = 'dev',
+--     args = { "cmd.exe ","/k", "C:\\DevSoftware\\msys64\\msys2_shell.cmd -defterm -here -no-start -msys2 -shell bash" },
+--     -- domain = {
+--     --   DomainName = 'bds16'
+--     -- }
+--   }
+
+--   local _, _, _ = dev_window:spawn_tab {
+--     domain = {
+--       DomainName = 'bds16'
+--     }
+--   }
+
+--   local _, _, _ = dev_window:spawn_tab {
+--     domain = {
+--       DomainName = 'bds60'
+--     }
+--   }
+
+--   -- local _, _, _ = dev_window:spawn_tab {
+--   --   args = { "cmd.exe ","/k", "C:\\DevSoftware\\msys64\\msys2_shell.cmd -defterm -here -no-start -msys2 -shell bash" },
+--   -- }
+
+--   local _, _, _ = dev_window:spawn_tab {
+--     domain = {
+--       DomainName = 'sds01'
+--     }
+--   }
+
+--   local _, _, _ = dev_window:spawn_tab {
+--     domain = {
+--       DomainName = 'sds05'
+--     }
+--   }
+
+--   local _, _, _ = dev_window:spawn_tab {
+--     domain = {
+--       DomainName = 'sds06'
+--     }
+--   }
+--  
+--   -- mux.set_active_workspace 'prod'
+--   mux.set_active_workspace 'dev'
+
+-- end)
+-- -- end
+
+-- -- Tmux shit
 config.leader = { key = "b", mods = "CTRL" }
 require("wez-tmux.plugin").apply_to_config(config, {})
+
+-- require("wez-pain-control.plugin").apply_to_config(config, {})
+-- require("wez-pain-control.plugin").apply_to_config(config, {
+--   pane_resize = 5,
+-- })
 
 -- and finally, return the configuration to wezterm
 return config
