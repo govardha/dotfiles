f8ab0631e1a6c5c72bfc789cabe7675342fe4454
rinku <govardha@me.com>
2025-08-16 19:53:05 +0000

    nvim clean up & refactor

--- nvim/lua/gov/plugins/lsp/mason.lua
+++ nvim/lua/gov/plugins/lsp/mason.lua
@@ -2,14 +2,18 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim", -- Add this dependency
   },
   config = function()
     -- import mason
     local mason = require("mason")
-
+    
     -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
-
+    
+    -- import mason-tool-installer
+    local mason_tool_installer = require("mason-tool-installer") 
+    
     -- enable mason and configure icons
     mason.setup({
       ui = {
@@ -20,20 +24,27 @@ return {
         },
       },
     })
-
+    
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
-        "emmet_ls",
-        "prismals",
-        -- "pyright",
         "basedpyright",
+        "bashls",
+        "awk_ls",
+        "ansiblels",
+        "jsonls",
+        "yamlls",
+        "docker_compose_language_service",
+        "lua_ls",
+      }
+    })
+    
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "shellcheck", -- Shell script linter
+        "ruff",       -- Python linter (you're already using this)
+        "stylua",     -- Lua formatter
+        "black",      -- Python formatter
       },
     })
   end,
--- nvim/lua/gov/plugins/which-key.lua
+++ nvim/lua/gov/plugins/which-key.lua
@@ -5,7 +5,7 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
-  opts = {
+ opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
