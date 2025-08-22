87e9f8736e983ec9d9e232a1d43913a3cc48aff7
Rinku@venkydepot <govardha@gmail.com>
2025-08-22 16:23:00 -0400

    added scripts

--- nvim.home/lua/gov/plugins/linting.lua
+++ nvim.home/lua/gov/plugins/linting.lua
@@ -1,31 +1,41 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "ruff" },
-      sh = { "shellcheck" },
-      bash = { "shellcheck" },
-    }
+		-- Helper function to check if executable exists in PATH
+		local function executable_exists(name)
+			return vim.fn.executable(name) == 1
+		end
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		-- Build linters_by_ft table based on available executables
+		local linters_by_ft = {}
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+		-- Python
+		if executable_exists("ruff") then
+			linters_by_ft.python = { "ruff" }
+		end
 
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+		-- Shell
+		if executable_exists("shellcheck") then
+			linters_by_ft.sh = { "shellcheck" }
+			linters_by_ft.bash = { "shellcheck" }
+		end
+
+		lint.linters_by_ft = linters_by_ft
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
 }
