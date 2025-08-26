0c0c5cf6c45d7af602a344dc7eb88addd5f30694
gov@what <govardha@me.com>
2025-08-26 23:49:42 +0000

    added treesitter extras

--- nvim.home/lua/gov/plugins/treesitter.lua
+++ nvim.home/lua/gov/plugins/treesitter.lua
@@ -4,23 +4,19 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	config = function()
-		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
 
-		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
+		treesitter.setup({
 			highlight = {
 				enable = true,
 			},
-			-- enable indentation
 			indent = { enable = true },
-			-- enable autotagging (w/ nvim-ts-autotag plugin)
 			autotag = {
 				enable = true,
 			},
-			-- ensure these language parsers are installed
 			ensure_installed = {
 				"json",
 				"yaml",
