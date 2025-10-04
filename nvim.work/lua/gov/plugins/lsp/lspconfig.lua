e799ad84a2406d8cd39b7e242f02d0264bb1ad2e
Govardhanen Gopal <govardha@me.com>
2025-10-04 11:32:13 -0400

    fixed lspconfig.lua

--- nvim.work/lua/gov/plugins/lsp/lspconfig.lua
+++ nvim.work/lua/gov/plugins/lsp/lspconfig.lua
@@ -15,52 +15,51 @@ return {
 			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 			callback = function(ev)
 				-- Buffer local mappings.
-				-- See `:help vim.lsp.*` for documentation on any of the below functions
 				local opts = { buffer = ev.buf, silent = true }
 
 				-- set keybinds
 				opts.desc = "Show LSP references"
-				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+				keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
 				opts.desc = "Go to declaration"
-				keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+				keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
 				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+				keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
 				opts.desc = "Show LSP implementations"
-				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+				keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
 				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
 				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
 				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
 				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
 				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
 				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
 				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
 				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+				keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
 				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
+		-- used to enable autocompletion
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		vim.diagnostic.config({
@@ -68,27 +67,34 @@ return {
 				text = {
 					[vim.diagnostic.severity.ERROR] = " ",
 					[vim.diagnostic.severity.WARN] = " ",
-					[vim.diagnostic.severity.HINT] = "󰠠 ",
+					[vim.diagnostic.severity.HINT] = "󰌵 ",
 					[vim.diagnostic.severity.INFO] = " ",
 				},
 			},
 		})
-
-		-- Get lspconfig
-		local lspconfig = require("lspconfig")
 
 		-- Helper function to check if executable exists in PATH
 		local function executable_exists(name)
 			return vim.fn.executable(name) == 1
 		end
 
-		-- Configure each LSP server if the executable exists
+		-- Configure lua_ls
 		if executable_exists("lua-language-server") or executable_exists("lua_ls") then
-			lspconfig.lua_ls.setup({
+			vim.lsp.config.lua_ls = {
+				cmd = { "lua-language-server" },
+				root_markers = {
+					".luarc.json",
+					".luarc.jsonc",
+					".luacheckrc",
+					".stylua.toml",
+					"stylua.toml",
+					"selene.toml",
+					"selene.yml",
+				},
+				filetypes = { "lua" },
 				capabilities = capabilities,
 				settings = {
 					Lua = {
-						-- make the language server recognize "vim" global
 						diagnostics = {
 							globals = { "vim" },
 						},
@@ -97,54 +103,91 @@ return {
 						},
 					},
 				},
-			})
+			}
+			vim.lsp.enable("lua_ls")
 		end
 
+		-- Configure basedpyright
 		if executable_exists("basedpyright") or executable_exists("pyright") then
-			lspconfig.basedpyright.setup({
+			vim.lsp.config.basedpyright = {
+				cmd = { "basedpyright-langserver", "--stdio" },
+				root_markers = {
+					"pyproject.toml",
+					"setup.py",
+					"setup.cfg",
+					"requirements.txt",
+					"Pipfile",
+					"pyrightconfig.json",
+				},
+				filetypes = { "python" },
 				capabilities = capabilities,
 				settings = {
 					basedpyright = {
-						-- Enable additional type checking features
 						analysis = {
-							typeCheckingMode = "basic", -- or "strict"
+							typeCheckingMode = "basic",
 							autoSearchPaths = true,
 							diagnosticMode = "workspace",
 							useLibraryCodeForTypes = true,
 						},
 					},
 				},
-			})
+			}
+			vim.lsp.enable("basedpyright")
 		end
 
+		-- Configure bashls
 		if executable_exists("bash-language-server") then
-			lspconfig.bashls.setup({
+			vim.lsp.config.bashls = {
+				cmd = { "bash-language-server", "start" },
+				root_markers = { ".git" },
+				filetypes = { "sh" },
 				capabilities = capabilities,
-			})
+			}
+			vim.lsp.enable("bashls")
 		end
 
+		-- Configure ansiblels
 		if executable_exists("ansible-language-server") then
-			lspconfig.ansiblels.setup({
+			vim.lsp.config.ansiblels = {
+				cmd = { "ansible-language-server", "--stdio" },
+				root_markers = { "ansible.cfg", ".ansible-lint" },
+				filetypes = { "yaml.ansible" },
 				capabilities = capabilities,
-			})
+			}
+			vim.lsp.enable("ansiblels")
 		end
 
+		-- Configure jsonls
 		if executable_exists("vscode-json-language-server") or executable_exists("json-languageserver") then
-			lspconfig.jsonls.setup({
+			vim.lsp.config.jsonls = {
+				cmd = { "vscode-json-language-server", "--stdio" },
+				root_markers = { ".git" },
+				filetypes = { "json", "jsonc" },
 				capabilities = capabilities,
-			})
+			}
+			vim.lsp.enable("jsonls")
 		end
 
+		-- Configure yamlls
 		if executable_exists("yaml-language-server") then
-			lspconfig.yamlls.setup({
+			vim.lsp.config.yamlls = {
+				cmd = { "yaml-language-server", "--stdio" },
+				root_markers = { ".git" },
+				filetypes = { "yaml", "yaml.docker-compose" },
 				capabilities = capabilities,
-			})
+			}
+			vim.lsp.enable("yamlls")
 		end
 
+		-- Configure docker_compose_language_service
 		if executable_exists("docker-compose-langserver") then
-			lspconfig.docker_compose_language_service.setup({
+			vim.lsp.config.docker_compose_language_service = {
+				cmd = { "docker-compose-langserver", "--stdio" },
+				root_markers = { "docker-compose.yml", "docker-compose.yaml", "compose.yml", "compose.yaml" },
+				filetypes = { "yaml.docker-compose" },
 				capabilities = capabilities,
-			})
+			}
+			vim.lsp.enable("docker_compose_language_service")
 		end
 	end,
 }
