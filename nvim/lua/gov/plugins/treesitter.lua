0a648ef6f14b0f76f098790e97b5a98f238fcf48
Govardhanen Gopal <govardha@me.com>
2025-12-29 07:56:45 -0500

    updated treesitter to pin to master

--- nvim/lua/gov/plugins/treesitter.lua
+++ nvim/lua/gov/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
 	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	dependencies = {
@@ -42,7 +43,7 @@ return {
 				"html",
 				"css",
 				"python",
-        "perl", 
+				"perl",
 				"markdown",
 				"markdown_inline",
 				"graphql",
