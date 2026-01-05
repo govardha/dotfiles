728608b5fb54cc0e03e8bc860736637af8286046
Govardhanen Gopal <govardha@me.com>
2026-01-05 12:33:44 -0500

    push nvim updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,15 @@
 require("gov.core")
 -- Filetype detection
 vim.filetype.add({
-	extension = {
-		d2 = "d2",
-	},
+  extension = {
+    d2 = "d2",
+  },
 })
+
+if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+  -- Force Unix line endings
+  vim.opt.fileformat = 'unix'
+  vim.opt.fileformats = 'unix,dos'
+end
 
 require("gov.lazy")
